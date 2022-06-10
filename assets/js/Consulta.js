9a093f8d1ac55bb635b2e4be1b0cb3d65305cbf2
Daniela Hernández <105611396+DanyBeth-Dev@users.noreply.github.com>
2022-06-09 22:39:25 -0400

    modificada mayuscula

--- assets/js/Consulta.js
+++ assets/js/Consulta.js
@@ -1,7 +1,7 @@
 //1. Para hacer la consulta asíncrona del archivo json:
 
 let personajes = ( () => {
-    const url = `${baseurl}/dbz.json`; //levantar según el puerto
+    const url = `torneo/dbz.json`; //levantar según el puerto
     const getData = async () => {
         const res = await fetch(url)
         const data = await res.json()
